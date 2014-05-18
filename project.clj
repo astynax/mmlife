b4d8f160fc6c84adc4b0f158055221996122b35f
Aleksei Pirogov <astynax@users.noreply.github.com>
2014-05-18 23:35:55 +0400

    cljs + websockets + basic GUI

--- project.clj
+++ project.clj
@@ -3,6 +3,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [http-kit "2.1.18"]]
-  :main mmlife.core)
-
+                 [org.clojure/clojurescript "0.0-2202"]
+                 [prismatic/dommy "0.1.2"]
+                 [compojure "1.1.8"]
+                 [http-kit "2.1.18"]
+                 [ring "1.2.1"]
+                 [sonian/carica "1.1.0" :exclusions [[cheshire]]]]
+  :plugins [[lein-cljsbuild "1.0.3"]]
+  :main mmlife.core
+  :cljsbuild {:builds [{:source-paths ["src-cljs"]
+                        :compiler {:output-to "resources/public/js/main.js"
+                                   :optimizations :whitespace
+                                   :pretty-print true}}]})
--- src/mmlife/core.clj
+++ src/mmlife/core.clj
@@ -1,15 +1,70 @@
 (ns mmlife.core
   (:gen-class true)
-  (:use org.httpkit.server))
+  (:require
+   [clojure.java.io :as io]
+   [org.httpkit.server :as server]
+   [compojure.handler :as ch]
+   [compojure.route :as cr]
+   [compojure.core :refer [defroutes GET POST DELETE ANY context]]
+   [carica.core :refer [config]]
+   [mmlife.cgol :as cgol]))
 
-(def PORT 8080)
+;; реестр каналов пользователей
+(defonce chans (atom {}))
 
-(defn app [req]
-  {:status  200
-   :headers {"Content-Type" "text/html"}
-   :body    "hello HTTP!"})
+;; хранилище состояния
+(defonce state (atom [;; glider на пустом поле
+                      (reduce #(cgol/insert %1 %2 1)
+                              (cgol/empty-field 128 128)
+                              [[1 0] [2 1] [0 2] [1 2] [2 2]])
+                      ;; слайсов на первом ходу нет
+                      {}]))
+
+;; делитель поля на области
+(def slice (partial cgol/split-field 8 8))
+
+;; функция обновления состояния
+(defn update-state!
+  [state]
+  (swap! state (fn [[field _]]
+                 (let [f (cgol/populate field)
+                       s (slice f)]
+                   [f s]))))
+
+;; цикл обновления состояния и отправки в соединения
+(defn updating-loop
+  [tout stop-flag chans state]
+  (loop []
+    (let [[_ slices] (update-state! state)]
+      (doseq [[ch k] @chans]
+        (server/send! ch (pr-str (slices (:pos k) '())))))
+    (Thread/sleep tout)
+    (when-not (realized? stop-flag)
+      (recur))))
+
+;; обработчик websocket-соединения
+(defn ws-handler
+  [request]
+  (server/with-channel request chan
+    (when (server/websocket? chan)
+      (swap! chans assoc chan {:pos [0 0]})
+      (server/on-close chan
+                       (fn [_] (swap! chans dissoc chan)))
+      (server/on-receive chan
+                         (fn [data]
+                           (println data))))))
+
+(defroutes app-routes
+  (GET "/" [] (slurp (io/resource "public/html/index.html")))
+  (GET "/ws" [] ws-handler)
+  (cr/resources "/static/")
+  (cr/not-found "Not found!"))
 
 (defn -main
   []
-  (println (str "Server started at " 8080))
-  (run-server app {:port PORT}))
+  (let [port (or (config :port) 8080)]
+    (println (str "Server started at " port))
+    (future (updating-loop 1000 (promise) chans state))
+    (server/run-server
+     (ch/site #'app-routes)
+     {:port port})))
